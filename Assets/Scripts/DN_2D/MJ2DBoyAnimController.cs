a00a5f37e4be20303b30baf8634f763fb7c4f9d2
JeongMyeongJin510 <audwls3159@gmail.com>
2026-05-16 15:26:33 +0900

    [Animation] 애니메이터 각 모션 추가,2D 캐릭터 키 입력시 이동 로직 추가

---
+++ Assets/Scripts/DN_2D/MJ2DBoyAnimController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+
+public enum AnimState
+{
+    None = 0,
+    Idle,
+    Walk,
+    Jump,
+    Atk
+}
+
+public class MJ2DBoyAnimController : MonoBehaviour
+{
+    [SerializeField] private Animator Animator_2DPlayer;
+
+    private AnimState _curState;
+
+    public void SetAnimState(AnimState newState)
+    {
+        _curState = newState;
+        ResetAllAnimParameters();
+
+        switch (_curState)
+        {
+            case AnimState.Idle:
+                break;
+            case AnimState.Walk:
+                Animator_2DPlayer.SetBool("IsWalk", true);
+                break;
+            case AnimState.Jump:
+                Animator_2DPlayer.SetBool("IsJump", true);
+                break;
+            case AnimState.Atk:
+                Animator_2DPlayer.SetBool("IsAtk", true);
+                break;
+
+            default:
+                ResetAllAnimParameters();
+                Debug.LogWarning("올바르지 않은, 작동되지 않은 상태입니다.");
+                break;
+        }
+    }
+
+    private void ResetAllAnimParameters()
+    {
+        Animator_2DPlayer.SetBool("IsWalk", false);
+        Animator_2DPlayer.SetBool("IsJump", false);
+        Animator_2DPlayer.SetBool("IsAtk", false);
+
+
+    }
+}
+

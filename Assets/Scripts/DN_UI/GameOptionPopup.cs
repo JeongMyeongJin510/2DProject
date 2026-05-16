a4d599bbb03d353a295e86cab63e961eda03ff99
JeongMyeongJin510 <audwls3159@gmail.com>
2026-05-16 16:55:37 +0900

    [UI] 게임옵션 UI 제작 및 스크립트 추가

---
+++ Assets/Scripts/DN_UI/GameOptionPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameOptionPopup : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Button_CloseSelfAllArea;
+
+    private void OnEnable()
+    {
+        Button_CloseSelfAllArea.BindOnClickButtonEvent(OnClick_ClosePopup);
+    }
+    public void OnClick_ClosePopup()
+    {
+        DaniTechUIManager.Instance.ClosePopupUI(DaniTechUIType.Popup_GameOption);
+    }
+}

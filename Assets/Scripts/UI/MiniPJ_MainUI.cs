a2ec0ae6527d4364c720b1e20b803bc089712fbf
JeongMyeongJin510 <audwls3159@gmail.com>
2026-05-18 18:41:46 +0900

    [UI] 미니프로젝트에 사용할 메인UI 코드 작성

---
+++ Assets/Scripts/UI/MiniPJ_MainUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MiniPJ_MainUI : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Btn_GameClose;
+    [SerializeField] private DaniTechUIButton Btn_GameBack;
+    [SerializeField] private DaniTechUIButton Btn_GameOption;
+
+    private void OnEnable()
+    {
+        Btn_GameClose.BindOnClickButtonEvent(OnClick_GameClose);
+        Btn_GameBack.BindOnClickButtonEvent(OnClick_GameBack);
+        Btn_GameOption.BindOnClickButtonEvent(OnClick_GameOption);
+    }
+
+    public void OnClick_GameClose()
+    {
+        Debug.LogWarning("눌러짐");
+    }
+
+    public void OnClick_GameBack()
+    {
+        DaniTechUIManager.Instance.OpenContentUI(DaniTechUIType.LobbyUI);
+        Debug.LogWarning("로비로 돌아가기");
+    }
+
+    public void OnClick_GameOption()
+    {
+        DaniTechUIManager.Instance.OpenGameOption();
+        Debug.LogWarning("게임 옵션 눌러짐");
+    }
+}

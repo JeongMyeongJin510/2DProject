0944b73d886c90101423a1de66d31ce23dec2c46
JeongMyeongJin510 <audwls3159@gmail.com>
2026-05-21 11:50:20 +0900

    [2D 프로젝트] 몬스터 - 데이터 드리븐, 몬스터 기초 코드 작성 몬스터 프리팹 만들기

---
+++ Assets/Scripts/DN_2D/GameMonster.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameMonster : MonsterBase
+{
+
+    private int _instanceId; // 게임 오브젝트 매니저에서 찾기 용 - 게임에 태어날때 부여되는 나의 주민번호
+    private string _dataId; // 데이터드리븐용 - 나의 부가정보를 나중에 찾을 수 있는 Id
+
+
+    public void InitMonster(int instanceId, string dataId)
+    {
+        _instanceId = instanceId;
+        _dataId = dataId;
+    }
+
+}
